61e1ed0c3deb72fa2f5f3b8c6843b418217a56fa
mahi-183 <Poojita38@gmail.com>
2019-08-14 19:23:02 +0530

    14Agust2019

--- UserModel/ApplicationUser.cs
+++ UserModel/ApplicationUser.cs
@@ -4,7 +4,6 @@
 // </copyright>
 // <creator name="Mahesh Aurad"/>
 // --------------------------------------------------------------------------------------------------------------------
-
 namespace UserModel
 {
     using Microsoft.AspNetCore.Identity;
---
+++ UserModel/NotesModel.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="NotesModel.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="Mahesh Aurad"/>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace UserModel
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class NotesModel
+    {
+        public string UserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the title.
+        /// </summary>
+        /// <value>
+        /// The title.
+        /// </value>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
+        /// <value>
+        /// The description.
+        /// </value>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user identifier.
+        /// </summary>
+        /// <value>
+        /// The user identifier.
+        /// </value>
+        public string Color { get; set; }
+    }
+}
